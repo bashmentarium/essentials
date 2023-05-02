d389227f650e7a1c4952b1b8ae2f88e95a8e751a
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-02 15:46:19 +0300

    [new] palindrome checker knows "mom" is a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,4 +1,9 @@
+import { PalindromeChecker } from "./index";
 
-describe('palindrome checker', () => {
+describe("palindrome checker", () => {
+  test('should be able to tell that "mom" is a palindrome', () => {
+    const palindromeChecker = new PalindromeChecker();
 
-})
+    expect(palindromeChecker.isAPalindrome("mom")).toBeTruthy();
+  });
+});
