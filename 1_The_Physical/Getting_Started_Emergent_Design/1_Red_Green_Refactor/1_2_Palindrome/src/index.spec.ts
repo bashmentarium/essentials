7e956a3dae145cb807b7f791bef82ac4aa02b574
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-02 15:50:44 +0300

    [new] palindromeChecker knows that "bill" is not a palindrome

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,9 +1,15 @@
 import { PalindromeChecker } from "./index";
 
 describe("palindrome checker", () => {
-  test('should be able to tell that "mom" is a palindrome', () => {
+  test('should be able to tell that "mom" or "wow" is a palindrome', () => {
     const palindromeChecker = new PalindromeChecker();
 
     expect(palindromeChecker.isAPalindrome("mom")).toBeTruthy();
+  });
+
+  test('should be able to tell that "bill" is not a palindrome', () => {
+    const palindromeChecker = new PalindromeChecker();
+
+    expect(palindromeChecker.isAPalindrome("bill")).toBeFalsy();
   });
 });
