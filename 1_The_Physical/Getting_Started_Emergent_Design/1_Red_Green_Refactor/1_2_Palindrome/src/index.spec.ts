bf4866ef09684fe9b3aacc3e974c4c06eb40620f
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-02 16:07:21 +0300

    [refactor] remove code repetitions

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_2_Palindrome/src/index.spec.ts
@@ -1,27 +1,25 @@
 import { PalindromeChecker } from "./index";
 
 describe("palindrome checker", () => {
-  test('should be able to tell that "mom" or "wow" is a palindrome', () => {
-    const palindromeChecker = new PalindromeChecker();
+  let palindromeChecker: PalindromeChecker;
 
+  beforeEach(() => {
+    palindromeChecker = new PalindromeChecker();
+  });
+
+  test('should be able to tell that "mom" or "wow" is a palindrome', () => {
     expect(palindromeChecker.isAPalindrome("mom")).toBeTruthy();
   });
 
   test('should be able to tell that "bill" is not a palindrome', () => {
-    const palindromeChecker = new PalindromeChecker();
-
     expect(palindromeChecker.isAPalindrome("bill")).toBeFalsy();
   });
 
   test("should be able to tell if palindrome regardless of the casing", () => {
-    const palindromeChecker = new PalindromeChecker();
-
     expect(palindromeChecker.isAPalindrome("Mom")).toBeTruthy();
   });
 
   test("should be able to tell a palindrome regardless of spacings", () => {
-    const palindromeChecker = new PalindromeChecker();
-
     expect(palindromeChecker.isAPalindrome("Was It A Rat I Saw")).toBeTruthy();
     expect(palindromeChecker.isAPalindrome("Never Odd or Even")).toBeTruthy();
   });
