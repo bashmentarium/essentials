1d946a95e7abe8edc3f2e73a41757963948c2367
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-02 15:16:36 +0300

    [refactor] divided complex test into separate condifitions

--- 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
+++ 1_The_Physical/Getting_Started_Emergent_Design/1_Red_Green_Refactor/1_1_Fizzbuzz/src/index.spec.ts
@@ -5,10 +5,16 @@ describe("fizzbuzz", () => {
     expect(typeof fizzBuzz(5)).toBe("string");
   });
 
-  test("takes a number from 1 to 100 as an argument and outputs it as a string", () => {
-    expect(fizzBuzz(11)).toBe("11");
+  it("doesnt accept numbers less than 1", () => {
     expect(() => fizzBuzz(0)).toThrow("Too small!");
+  });
+
+  it("doesnt accept numbers greater than 100", () => {
     expect(() => fizzBuzz(101)).toThrow("Too large!");
+  });
+
+  test("outputs numbers as strings", () => {
+    expect(fizzBuzz(11)).toBe("11");
   });
 
   test("returns 'Fizz' if input number is a multiple of 3", () => {
