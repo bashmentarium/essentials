fc6009b414a4dedd8560e0184b86e736728823b9
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-03 15:54:14 +0300

    [new] create validator

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,5 +1,9 @@
+import { TimeValidator } from "./index";
 
-describe('military time validator', () => {
+describe("military time validator", () => {
+  test("should exist", () => {
+    const timeValidator = new TimeValidator();
 
-
-})
+    expect(timeValidator.validateTime("22:00 - 11:00")).toBeDefined();
+  });
+});
---
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.ts
@@ -0,0 +1,5 @@
+export class TimeValidator {
+  validateTime(time: string) {
+    return time;
+  }
+}
