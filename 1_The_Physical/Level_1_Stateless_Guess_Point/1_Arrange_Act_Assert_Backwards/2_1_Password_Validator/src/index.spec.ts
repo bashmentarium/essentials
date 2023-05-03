47c2f2d8c398a0700c571db8be62f80ad852fe91
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-03 10:09:36 +0300

    [refactor] remove duplacates in tests

--- 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/1_Arrange_Act_Assert_Backwards/2_1_Password_Validator/src/index.spec.ts
@@ -1,21 +1,21 @@
 import { PasswordChecker } from "./index";
 
 describe("password validator", () => {
-  test("exists", () => {
-    const passwordChecker = new PasswordChecker();
+  let passwordChecker: PasswordChecker;
 
+  beforeEach(() => {
+    passwordChecker = new PasswordChecker();
+  });
+
+  test("exists", () => {
     expect(passwordChecker.checkPassword("mo7herS")).toBeTruthy();
   });
 
   test("returns an invalid length error when strings like 'mom' are less than 5 characters", () => {
-    const passwordChecker = new PasswordChecker();
-
     expect(passwordChecker.checkPassword("mom")).toBe("InvalidLengthError");
   });
 
   test("returns an invalid length error when strings like 'dsadnwdqdwijdqwdiqjwd' are more than 15 characters", () => {
-    const passwordChecker = new PasswordChecker();
-
     expect(passwordChecker.checkPassword("dsadnwdqdwijdqwdiqjwd")).toBe(
       "InvalidLengthError"
     );
