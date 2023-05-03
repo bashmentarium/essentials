3579dac4244d7e984f57a62aab499d6759feeab2
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-03 17:30:25 +0300

    [refactor] remove code repetitions

--- 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
+++ 1_The_Physical/Level_1_Stateless_Guess_Point/2_Manage_Complexity/3_1_Military_Time_Validator/src/index.spec.ts
@@ -1,39 +1,33 @@
 import { MilitaryTimeValidator } from "./index";
 
 describe("military time validator", () => {
-  it('knows that "01:00 - 11:00" is a valid military time', () => {
-    const militaryTimeValidator = new MilitaryTimeValidator();
+  let militaryTimeValidator: MilitaryTimeValidator;
 
+  beforeEach(() => {
+    militaryTimeValidator = new MilitaryTimeValidator();
+  });
+
+  it('knows that "01:00 - 11:00" is a valid military time', () => {
     expect(militaryTimeValidator.validate("01:00 - 11:00")).toBeTruthy();
   });
 
   test("knows that an empty string is not a valid time", () => {
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
     expect(militaryTimeValidator.validate("")).toBeFalsy();
   });
 
   test("knows that '010011:00' is not a valid military time", () => {
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
     expect(militaryTimeValidator.validate("010011:00")).toBeFalsy();
   });
 
   it('knows that "0100- 1:00" is not a valid military time', () => {
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
     expect(militaryTimeValidator.validate("0100- 1:00")).toBeFalsy();
   });
 
   it('knows that "1:00 - 0100" is not a valid military time', () => {
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
     expect(militaryTimeValidator.validate("1:00 - 0100")).toBeFalsy();
   });
 
   it('knows that "0100 - 01:00" is not a valid military time', () => {
-    const militaryTimeValidator = new MilitaryTimeValidator();
-
     expect(militaryTimeValidator.validate("0100 - 01:00")).toBeFalsy();
   });
 });
