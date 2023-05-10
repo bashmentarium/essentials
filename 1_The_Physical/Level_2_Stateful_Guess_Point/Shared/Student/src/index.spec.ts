8263527acc21cc6e3c20523bd26b59e56aa6b1d6
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-10 11:36:59 +0300

    [new] student class makes use of arguments

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.spec.ts
@@ -1,5 +1,23 @@
+import { Student } from "./index";
 
-describe('', () => {
+describe("Student State Machine", () => {
+  it("accepts 'firstName' and 'lastName' as arguments", () => {
+    const firstName = "John";
+    const lastName = "Doe";
 
+    const studentStateMachine = new Student(firstName, lastName);
 
-})
+    expect(studentStateMachine.firstName).toBeDefined();
+    expect(studentStateMachine.lastName).toBeDefined();
+  });
+
+  it("assigns 'firstName' and 'lastName' arguments as instance properties", () => {
+    const firstName = "John";
+    const lastName = "Doe";
+
+    const studentStateMachine = new Student(firstName, lastName);
+
+    expect(studentStateMachine.firstName).toEqual(firstName);
+    expect(studentStateMachine.lastName).toEqual(lastName);
+  });
+});
