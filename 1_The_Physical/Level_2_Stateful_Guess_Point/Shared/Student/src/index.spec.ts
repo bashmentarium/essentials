7ea677148cf1b56afd03555135634a25f72b8528
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-10 12:09:03 +0300

    [refactor] extract variables in tests

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.spec.ts
@@ -1,33 +1,31 @@
 import { Student, ERROR_MSG } from "./index";
 
 describe("Student State Machine", () => {
+  const firstName = "John";
+  const lastName = "Doe";
+  const invalidFirstName = "";
+  const invalidLastName = "";
+  let studentStateMachine: Student;
+
+  beforeEach(() => {
+    studentStateMachine = new Student(firstName, lastName);
+  });
+
   it("accepts 'firstName' and 'lastName' as arguments", () => {
-    const firstName = "John";
-    const lastName = "Doe";
-
-    const studentStateMachine = new Student(firstName, lastName);
-
     expect(studentStateMachine.firstName).toBeDefined();
     expect(studentStateMachine.lastName).toBeDefined();
   });
 
   it("assigns 'firstName' and 'lastName' arguments as instance properties", () => {
-    const firstName = "John";
-    const lastName = "Doe";
-
-    const studentStateMachine = new Student(firstName, lastName);
-
     expect(studentStateMachine.firstName).toEqual(firstName);
     expect(studentStateMachine.lastName).toEqual(lastName);
   });
 
   it("throws error when no 'firstName' or 'lastName' provided", () => {
-    const firstName = "John";
-    const lastName = "Doe";
-    const invalidFirstName = "";
-    const invalidLastName = "";
-
     expect(() => new Student(invalidFirstName, lastName)).toThrow(ERROR_MSG);
     expect(() => new Student(firstName, invalidLastName)).toThrow(ERROR_MSG);
+    expect(() => new Student(invalidFirstName, invalidLastName)).toThrow(
+      ERROR_MSG
+    );
   });
 });
