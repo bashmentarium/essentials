d1533d330ec0e99e620a4b127664fefbbc3fe7a6
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-11 09:07:58 +0300

    [refactor] split tests

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/Student/src/index.spec.ts
@@ -1,62 +1,66 @@
 import { Student, ERROR_MSG } from "./index";
 
 describe("Student State Machine", () => {
-  test("accepts 'firstName' and 'lastName' as arguments", () => {
-    const firstName = "John";
-    const lastName = "Doe";
-    const studentStateMachine = new Student(firstName, lastName);
+  describe("functionality", () => {
+    test("accepts 'firstName' and 'lastName' as arguments", () => {
+      const firstName = "John";
+      const lastName = "Doe";
+      const studentStateMachine = new Student(firstName, lastName);
 
-    expect(studentStateMachine.firstName).toBeDefined();
-    expect(studentStateMachine.lastName).toBeDefined();
+      expect(studentStateMachine.firstName).toBeDefined();
+      expect(studentStateMachine.lastName).toBeDefined();
+    });
+
+    test("assigns 'firstName' and 'lastName' arguments as instance properties", () => {
+      const firstName = "John";
+      const lastName = "Doe";
+      const studentStateMachine = new Student(firstName, lastName);
+
+      expect(studentStateMachine.firstName).toEqual(firstName);
+      expect(studentStateMachine.lastName).toEqual(lastName);
+    });
   });
 
-  test("assigns 'firstName' and 'lastName' arguments as instance properties", () => {
-    const firstName = "John";
-    const lastName = "Doe";
-    const studentStateMachine = new Student(firstName, lastName);
+  describe("throws an error", () => {
+    test("when no 'firstName' or 'lastName' provided", () => {
+      const firstName = "John";
+      const lastName = "Doe";
+      const invalidFirstName = "";
+      const invalidLastName = "";
 
-    expect(studentStateMachine.firstName).toEqual(firstName);
-    expect(studentStateMachine.lastName).toEqual(lastName);
-  });
+      expect(() => new Student(invalidFirstName, lastName)).toThrow(ERROR_MSG);
+      expect(() => new Student(firstName, invalidLastName)).toThrow(ERROR_MSG);
+      expect(() => new Student(invalidFirstName, invalidLastName)).toThrow(
+        ERROR_MSG
+      );
+    });
 
-  test("throws error when no 'firstName' or 'lastName' provided", () => {
-    const firstName = "John";
-    const lastName = "Doe";
-    const invalidFirstName = "";
-    const invalidLastName = "";
+    test("when 'firstName' is shorter than 2 characters", () => {
+      const invalidFirstName = "J";
+      const lastName = "Doe";
 
-    expect(() => new Student(invalidFirstName, lastName)).toThrow(ERROR_MSG);
-    expect(() => new Student(firstName, invalidLastName)).toThrow(ERROR_MSG);
-    expect(() => new Student(invalidFirstName, invalidLastName)).toThrow(
-      ERROR_MSG
-    );
-  });
+      expect(() => new Student(invalidFirstName, lastName)).toThrow(ERROR_MSG);
+    });
 
-  test("throws error when 'firstName' is shorter than 2 characters", () => {
-    const invalidFirstName = "J";
-    const lastName = "Doe";
+    test("when 'firstName' is longer than 10 characters", () => {
+      const invalidFirstName = "JohnJohnJohn";
+      const lastName = "Doe";
 
-    expect(() => new Student(invalidFirstName, lastName)).toThrow(ERROR_MSG);
-  });
+      expect(() => new Student(invalidFirstName, lastName)).toThrow(ERROR_MSG);
+    });
 
-  test("throws error when 'firstName' is longer than 10 characters", () => {
-    const invalidFirstName = "JohnJohnJohn";
-    const lastName = "Doe";
+    test("when 'lastName' is shorter than 2 characters", () => {
+      const firstName = "John";
+      const invalidLastName = "A";
 
-    expect(() => new Student(invalidFirstName, lastName)).toThrow(ERROR_MSG);
-  });
+      expect(() => new Student(firstName, invalidLastName)).toThrow(ERROR_MSG);
+    });
 
-  test("throws error when 'lastName' is shorter than 2 characters", () => {
-    const firstName = "John";
-    const invalidLastName = "A";
+    test("when 'lastName' is longer than 15 characters", () => {
+      const firstName = "John";
+      const invalidLastName = "DoeDoeDoeDoeDoeDoe";
 
-    expect(() => new Student(firstName, invalidLastName)).toThrow(ERROR_MSG);
-  });
-
-  test("throws error when 'lastName' is longer than 15 characters", () => {
-    const firstName = "John";
-    const invalidLastName = "DoeDoeDoeDoeDoeDoe";
-
-    expect(() => new Student(firstName, invalidLastName)).toThrow(ERROR_MSG);
+      expect(() => new Student(firstName, invalidLastName)).toThrow(ERROR_MSG);
+    });
   });
 });
