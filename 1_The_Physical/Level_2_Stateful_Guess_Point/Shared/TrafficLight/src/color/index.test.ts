1807b9f1f9c9e309471497bc25a36f91ff1dced2
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-17 16:38:03 +0300

    [Refactor] use a constant for color possible values

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/color/index.test.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/color/index.test.ts
@@ -2,20 +2,20 @@ import { Color, ColorOptions } from "./index";
 
 describe("Color", () => {
   it("can return RED color option", () => {
-    const redColor: Color = Color.create(ColorOptions.RED);
+    const redColor = Color.create(ColorOptions.RED);
 
-    expect(redColor.value).toBe("RED");
+    expect(redColor.value).toBe(ColorOptions.RED);
   });
 
   it("can return YELLOW color option", () => {
-    const yellowColor: Color = Color.create(ColorOptions.YELLOW);
+    const yellowColor = Color.create(ColorOptions.YELLOW);
 
-    expect(yellowColor.value).toBe("YELLOW");
+    expect(yellowColor.value).toBe(ColorOptions.YELLOW);
   });
 
   it("can return GREEN color option", () => {
-    const greenColor: Color = Color.create(ColorOptions.GREEN);
+    const greenColor = Color.create(ColorOptions.GREEN);
 
-    expect(greenColor.value).toBe("GREEN");
+    expect(greenColor.value).toBe(ColorOptions.GREEN);
   });
 });
