04b2b861f4a93152f00e011706bfca36a1dace73
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-18 13:09:17 +0300

    [Refactor] remove duplicate/unnecessary code in coordinator test

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/coordinator/index.test.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TrafficLight/src/coordinator/index.test.ts
@@ -3,25 +3,24 @@ import { TrafficLight } from "../traffic_light";
 import { Color, ColorOptions } from "../color";
 
 describe("Coordinator", () => {
-  it("should have a 'secondsPassed' property", () => {
-    const trafficLight = TrafficLight.create();
-    const coordinator = Coordinator.createAndStart(trafficLight);
+  let trafficLight: TrafficLight;
+  let coordinator: Coordinator;
 
-    const redColor = Color.create(ColorOptions.RED);
+  let redColor: Color = Color.create(ColorOptions.RED);
+  let yellowColor: Color = Color.create(ColorOptions.YELLOW);
+  let greenColor: Color = Color.create(ColorOptions.GREEN);
+
+  beforeAll(() => {
+    trafficLight = TrafficLight.create();
+    coordinator = Coordinator.createAndStart(trafficLight);
     trafficLight.turnOn(redColor);
-
-    expect(coordinator.secondsPassed).toBeDefined();
   });
 
-  it("should pass 'YELLOW' to Traffic Light after 30 seconds", () => {
-    const trafficLight = TrafficLight.create();
-    const coordinator = Coordinator.createAndStart(trafficLight);
+  it("should have a 'secondsPassed' property", () => {
+    expect(coordinator.getSecondsPassed).toBeDefined();
+  });
 
-    const redColor = Color.create(ColorOptions.RED);
-    const yellowColor = Color.create(ColorOptions.YELLOW);
-
-    trafficLight.turnOn(redColor);
-
-    expect(coordinator.secondsPassed).toEqual(30);
+  afterAll(() => {
+    Coordinator.stopTimer(coordinator);
   });
 });
