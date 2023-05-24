13e72f1c9a568168ee078d03b9a81a89c29dd025
bashmentarium <mihigh.ciobanu@gmail.com>
2023-05-24 10:00:33 +0300

    [New] add start and stop methods to the game

--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/game/index.spec.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/game/index.spec.ts
@@ -1,13 +1,29 @@
 import { TicTacToe } from "./index";
 
 describe("Tic Tac Toe Game", () => {
-  it("should have a 3 x 3 board ", () => {
-    const ticTacToe = new TicTacToe();
+  it("should be able to have a 'start' method", () => {
+    const game = new TicTacToe();
 
-    expect(ticTacToe.board).toBeDefined();
-    expect(ticTacToe.board.length).toBe(3);
-    expect(ticTacToe.board[0].length).toBe(3);
-    expect(ticTacToe.board[1].length).toBe(3);
-    expect(ticTacToe.board[2].length).toBe(3);
+    game.start();
+
+    expect(game.status).toEqual("started");
+  });
+
+  it("should be able to have a 'stop' method", () => {
+    const game = new TicTacToe();
+
+    game.stop();
+
+    expect(game.status).toEqual("stopped");
+  });
+
+  it("should have a 3 x 3 board ", () => {
+    const game = new TicTacToe();
+
+    expect(game.board).toBeDefined();
+    expect(game.board.length).toBe(3);
+    expect(game.board[0].length).toBe(3);
+    expect(game.board[1].length).toBe(3);
+    expect(game.board[2].length).toBe(3);
   });
 });
--- 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/game/index.ts
+++ 1_The_Physical/Level_2_Stateful_Guess_Point/Shared/TicTacToe/src/game/index.ts
@@ -1,5 +1,6 @@
 export class TicTacToe {
   board: string[][];
+  status: string;
 
   constructor() {
     this.board = [
@@ -7,5 +8,24 @@ export class TicTacToe {
       ["", "", ""],
       ["", "", ""],
     ];
+    this.status = "stopped";
+  }
+
+  public start() {
+    this.board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+    this.status = "started";
+  }
+
+  public stop() {
+    this.board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
+    this.status = "stopped";
   }
 }
